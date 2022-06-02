042cb5659584bd4d05fd00ba26d19cbf90f9ba0e
Adrien GIRARD <adrien.girard@wabtec.com>
2022-06-02 15:06:46 +0200

    update fridge class

--- core/class/fridge.class.php
+++ core/class/fridge.class.php
@@ -18,7 +18,7 @@
 /* * ***************************Includes********************************* */
 require_once __DIR__  . '/../../../../core/php/core.inc.php';
 
-class template extends eqLogic {
+class fridge extends eqLogic {
   /*     * *************************Attributs****************************** */
 
   /*
@@ -141,7 +141,7 @@ class template extends eqLogic {
 
 }
 
-class templateCmd extends cmd {
+class fridgeCmd extends cmd {
   /*     * *************************Attributs****************************** */
 
   /*
